65cbfa798c6b56e4e5434ca8fd62938a7bf42343
AD DAB <adamtareq138@gmail.com>
2022-12-12 00:37:59 +0300

    adding accepting transaction on the other side

--- src/instruction.rs
+++ src/instruction.rs
@@ -10,9 +10,28 @@ pub enum EscrowInstruction {
     /// 2. `[]` The initializer's token account for the token they will receive should the trade go through
     /// 3. `[writable]` The escrow account, it will hold all necessary info about the trade.
     /// 4. `[]` The rent sysvar
-    /// 5. `[]` The token program
+    /// 5. `[]` The token src
     InitEscrow {
         /// The amount party A expects to receive of token Y
+        amount: u64,
+    },
+
+    /// Accepts a trade
+    ///
+    ///
+    /// Accounts expected:
+    ///
+    /// 0. `[signer]` The account of the person taking the trade
+    /// 1. `[writable]` The taker's token account for the token they send
+    /// 2. `[writable]` The taker's token account for the token they will receive should the trade go through
+    /// 3. `[writable]` The PDA's temp token account to get tokens from and eventually close
+    /// 4. `[writable]` The initializer's main account to send their rent fees to
+    /// 5. `[writable]` The initializer's token account that will receive tokens
+    /// 6. `[writable]` The escrow account holding the escrow info
+    /// 7. `[]` The token program
+    /// 8. `[]` The PDA account
+    Exchange {
+        /// The amount party B expects to receive of token X
         amount: u64,
     },
 }
--- src/processor.rs
+++ src/processor.rs
@@ -1,13 +1,16 @@
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
-    msg,
     program_error::ProgramError,
+    msg,
     pubkey::Pubkey,
+    program_pack::{Pack, IsInitialized},
     sysvar::{rent::Rent, Sysvar},
+    program::invoke
 };
 
 use crate::{errors::EscrowError, instruction::EscrowInstruction, state::Escrow};
+use crate::errors::EscrowError::InvalidInstruction;
 
 pub struct Processor;
 impl Processor {
@@ -18,12 +21,15 @@ impl Processor {
     ) -> ProgramResult {
         let instruction = EscrowInstruction::unpack(instruction_data)?;
 
-        match instruction {
-            EscrowInstruction::InitEscrow { amount } => {
-                msg!("Instruction: InitEscrow");
-                Self::process_init_escrow(accounts, amount, program_id)
-            }
-        }
+        Ok(match tag {
+            0 => Self::InitEscrow {
+                amount: Self::unpack_amount(rest)?,
+            },
+            1 => Self::Exchange {
+                amount: Self::unpack_amount(rest)?
+            },
+            _ => return Err(InvalidInstruction.into()),
+        })
     }
 
     fn process_init_escrow(
@@ -51,6 +57,37 @@ impl Processor {
         if escrow_info.is_initialized {
             return Err(ProgramError::AccountAlreadyInitialized);
         }
+        escrow_info.is_initialized = true;
+        escrow_info.initializer_pubkey = *initializer.key;
+        escrow_info.temp_token_account_pubkey = *temp_token_account.key;
+        escrow_info.initializer_token_to_receive_account_pubkey = *token_to_receive_account.key;
+        escrow_info.expected_amount = amount;
+
+        Escrow::pack(escrow_info, &mut escrow_account.try_borrow_mut_data()?)?;
+        let (pda, _bump_seed) = Pubkey::find_program_address(&[b"escrow"], program_id);
+
+        let token_program = next_account_info(account_info_iter)?;
+        let owner_change_ix = spl_token::instruction::set_authority(
+            token_program.key,
+            temp_token_account.key,
+            Some(&pda),
+            spl_token::instruction::AuthorityType::AccountOwner,
+            initializer.key,
+            &[&initializer.key],
+        )?;
+
+        msg!("Calling the token src to transfer token account ownership...");
+        invoke(
+            &owner_change_ix,
+            &[
+                temp_token_account.clone(),
+                initializer.clone(),
+                token_program.clone(),
+            ],
+        )?;
+
+
+
         Ok(())
     }
 }
